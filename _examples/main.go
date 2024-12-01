b78062dc9fd3b5ae7fa8ce3483a97e1a96dc5eec
eric <11235171+eolso@users.noreply.github.com>
2024-11-30 18:13:55 -0800

    Update to new aki api

--- _examples/main.go
+++ _examples/main.go
@@ -2,71 +2,40 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/eolso/akiapi"
 )
 
 func main() {
-	// This might need to be uncommented depending on the site's status/your os' certs
-	//akiapi.SetHttpClient(&http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}})
+	var session akiapi.SessionManager
+	session = akiapi.NewClient()
 
-	game, err := akiapi.NewGame(akiapi.GameOptions{Theme: akiapi.CharactersTheme, Language: akiapi.English})
-	if err != nil {
+	if err := session.NewGame(akiapi.ThemeCharacters, false); err != nil {
 		panic(err)
 	}
 
-	var response int
-	questionBuffer := 0
+	var response string
 	for {
-		if game.Progress() > 80.0 && questionBuffer <= 0 {
-			guess, err := game.Guess()
-			if err != nil {
+		if session.IsAnswered() {
+			fmt.Println(session.Answer().Name)
+			if err := session.AcceptAnswer(); err != nil {
 				panic(err)
 			}
-			fmt.Printf("Is it: %s?\n", guess.Name())
-			fmt.Println("1) Yes")
-			fmt.Println("2) No")
-			fmt.Scanln(&response)
-
-			if response == 1 {
-				fmt.Println("Ayy lmao")
-				return
-			} else if response == 2 {
-				questionBuffer = 5
-				continue
-			}
+			return
 		}
 
-		fmt.Println(game.Question())
-		for index, answer := range game.Options() {
-			fmt.Printf("%d) %s\n", index+1, answer)
-		}
-
+		fmt.Println(session.Question())
 		fmt.Printf("> ")
 		fmt.Scanln(&response)
 
-		switch response {
-		case 0:
-			if err = game.Undo(); err != nil {
+		if response == "u" {
+			if err := session.UndoResponse(); err != nil {
 				panic(err)
 			}
-		case 1, 2, 3, 4, 5:
-			if err = game.SelectOption(response - 1); err != nil {
+		} else {
+			if err := session.Respond(akiapi.Response(strings.TrimSpace(response))); err != nil {
 				panic(err)
-			}
-			questionBuffer--
-		case 6:
-			guesses, err := game.ListGuesses()
-			if err != nil {
-				panic(err)
-			}
-
-			for i, guess := range guesses {
-				fmt.Printf("    %d) %s [%02f]\n", i+1, guess.Name(), guess.Probability())
-			}
-		case 7:
-			for i, r := range game.Responses() {
-				fmt.Printf("    %d) %s %s\n", i+1, r.Question, r.Answer)
 			}
 		}
 	}
---
+++ session.go
@@ -0,0 +1,46 @@
+package akiapi
+
+type SessionManager interface {
+	NewGame(theme Theme, childMode bool) error
+
+	Respond(response Response) error
+	UndoResponse() error
+
+	Question() string
+	History() []QuestionAnswer
+
+	IsAnswered() bool
+	Answer() Answer
+	AcceptAnswer() error
+	DeclineAnswer() error
+}
+
+type Theme string
+
+const (
+	ThemeCharacters Theme = "1"
+	ThemeObjects    Theme = "2"
+	ThemeAnimals    Theme = "14"
+)
+
+type Response string
+
+const (
+	ResponseYes         Response = "0"
+	ResponseNo          Response = "1"
+	ResponseDontKnow    Response = "2"
+	ResponseProbably    Response = "3"
+	ResponseProbablyNot Response = "4"
+)
+
+type Answer struct {
+	Id          string
+	Name        string
+	Description string
+	PhotoUrl    string
+}
+
+type QuestionAnswer struct {
+	Question string
+	Answer   Response
+}
